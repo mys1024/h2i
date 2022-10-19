9a158fcfa8ed6c2ab7bed489d82de0cae69e1322
mys1024 <mys.1024@outlook.com>
2022-10-19 13:57:12 +0800

    feat: save buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { toPng } from 'html-to-image'
-import { Show, createSignal } from 'solid-js'
+import { toJpeg, toPng, toSvg } from 'html-to-image'
+import { createSignal } from 'solid-js'
 import Editor from '~/components/Editor'
 import Footer from '~/components/Footer'
 
@@ -16,77 +16,95 @@ const defaultCss = `.box {
 }
 `
 
-const [html, setHtml] = createSignal(defaultHtml)
-const [css, setCss] = createSignal(defaultCss)
-const [previewSrc, setPreviewSrc] = createSignal('')
+export default () => {
+  const [html, setHtml] = createSignal(defaultHtml)
+  const [css, setCss] = createSignal(defaultCss)
+  const [url, setUrl] = createSignal('')
+  let anchor: HTMLAnchorElement
 
-async function updatePreview() {
-  const url = await toPng(sandbox)
-  setPreviewSrc(url)
-}
+  async function save(type: 'png' | 'jpg' | 'svg') {
+    switch (type) {
+      case 'png':
+        setUrl(await toPng(sandbox))
+        break
+      case 'jpg':
+        setUrl(await toJpeg(sandbox))
+        break
+      case 'svg':
+        setUrl(await toSvg(sandbox))
+        break
+      default:
+        throw new Error(`Unknown type: ${type}`)
+    }
+    anchor.click()
+  }
 
-export default () => (
-  <div
-    space-y-12
-    flex flex-col
-    min-h-100vh
-    items-center
-    bg="#282c34"
-    text-white
-  >
-    {/* sandbox */}
-    <div>
-      <div
-        id="sandbox"
-        ref={sandbox}
-        // eslint-disable-next-line solid/no-innerhtml
-        innerHTML={html()}
-      />
-      <style
-        // eslint-disable-next-line solid/no-innerhtml
-        innerHTML={css()}
-      />
-    </div>
-    {/* preview */}
-    <Show
-      when={previewSrc()}
-    >
-      <img
-        id="preview"
-        alt="preview"
-        src={previewSrc()}
-      />
-    </Show>
-    {/* button */}
-    <div>
-      <button
-        px-2 py-1
-        rounded
-        transition-250
-        bg="gray hover:sky"
-        onClick={updatePreview}
-      >
-        Preview
-      </button>
-    </div>
-    {/* editors */}
+  return (
     <div
-      w-full h-120
-      p-8
-      grid grid-rows-1 grid-cols-2 gap-8
+      space-y-12
+      flex flex-col
+      min-h-100vh
+      items-center
+      bg="#282c34"
+      text-white
     >
-      <Editor
-        lang='html'
-        code={html()}
-        update={code => setHtml(code)}
-      />
-      <Editor
-        lang='css'
-        code={css()}
-        update={code => setCss(code)}
-      />
+      {/* sandbox */}
+      <div>
+        <div
+          id="sandbox"
+          ref={sandbox}
+          // eslint-disable-next-line solid/no-innerhtml
+          innerHTML={html()}
+        />
+        <style
+          // eslint-disable-next-line solid/no-innerhtml
+          innerHTML={css()}
+        />
+        <a ref={anchor} href={url()} download="image" />
+      </div>
+      {/* button */}
+      <div space-x-4>
+        <button
+          px-2 py-1
+          rounded transition-250 bg="gray hover:sky"
+          onClick={() => save('png')}
+        >
+          Save as PNG
+        </button>
+        <button
+          px-2 py-1
+          rounded transition-250 bg="gray hover:sky"
+          onClick={() => save('jpg')}
+        >
+          Save as JPG
+        </button>
+        <button
+          px-2 py-1
+          rounded transition-250 bg="gray hover:sky"
+          onClick={() => save('svg')}
+        >
+          Save as SVG
+        </button>
+      </div>
+      {/* editors */}
+      <div
+        w-full h-120
+        p-8
+        grid grid-rows-1 grid-cols-2 gap-8
+      >
+        <Editor
+          lang='html'
+          code={html()}
+          update={code => setHtml(code)}
+        />
+        <Editor
+          lang='css'
+          code={css()}
+          update={code => setCss(code)}
+        />
+      </div>
+      {/* footer */}
+      <Footer />
     </div>
-    {/* footer */}
-    <Footer />
-  </div>
-)
+  )
+}
